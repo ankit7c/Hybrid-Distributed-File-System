208c78afe9b346ef4228b9b2e4ff6b65e36faf14
ankit7c <chavanankit7@gmail.com>
2024-11-08 10:22:38 -0600

    Added input files and changes to bat file

--- run.bat
+++ run.bat
@@ -1,14 +1,14 @@
 @echo off
 
 REM Define lists as comma-separated values
-set HOSTS=fa24-cs425-a605.cs.illinois.edu
-set IPS=172.22.159.98
-set PORTS1=5005
-set PORTS2=6005
-set NAMES=Machine5
+set HOSTS=fa24-cs425-a606.cs.illinois.edu,fa24-cs425-a607.cs.illinois.edu,fa24-cs425-a608.cs.illinois.edu,fa24-cs425-a609.cs.illinois.edu,fa24-cs425-a610.cs.illinois.edu
+set IPS=172.22.95.97,172.22.157.98,172.22.159.99,172.22.95.98,172.22.157.99
+set PORTS1=5006,5007,5008,5009,5010
+set PORTS2=6006,6007,6008,6009,6010
+set NAMES=Machine6,Machine7,Machine8,Machine9,Machine10
 
 REM Define variables
-set VM_USER=sdare1
+set VM_USER=auc3
 set /p VM_PASSWORD=< password.txt
 
 REM Split the lists into arrays and iterate over them
@@ -60,14 +60,13 @@ for /L %%i in (1,1,%max_index%) do (
         echo.
     ) | plink %VM_USER%@!VM_HOST! -l %VM_USER% -pw %VM_PASSWORD% ^
         "pwd && " ^
-        "mkdir Distributed && " ^
         "cd Distributed && " ^
         "git init && " ^
-        "git config user.name 'sdare1' && " ^
-        "git config user.email 'sdare1@illinois.edu' && " ^
-        "git clone https://%VM_USER%:%VM_PASSWORD%@gitlab.engr.illinois.edu/sdare1/GA6-CS-425-MP2.git && " ^
+        "git config user.name 'auc3' && " ^
+        "git config user.email 'auc3@illinois.edu' && " ^
+        "git clone https://%VM_USER%:%VM_PASSWORD%@gitlab.engr.illinois.edu/sdare1/Hybrid-Distributed-File-System.git && " ^
         "ls && " ^
-        "cd GA6-CS-425-MP2 && " ^
+        "cd Hybrid-Distributed-File-System && " ^
         "mvn install -DskipTests && " ^
         "echo 'machineIp=!MACHINE_IP!' >> application.properties && " ^
         "echo 'port.number=!PORT_NUMBER1!' >> application.properties && " ^
